c734212c0cee3d5f57cd528d51794a73d8a6562a
Yiwei Lin <s921975628@gmail.com>
2025-10-20 00:21:41 +0800

    Get information from block_io_start

--- bpf/biomon.bpf.c
+++ bpf/biomon.bpf.c
@@ -10,10 +10,30 @@
 
 #include "common.h"
 
-SEC("tracepoint/block/block_rq_insert")
-int sys_enter(UNUSED struct pt_regs *ctx)
+#define MINORBITS 20
+#define MINORMASK ((1U << MINORBITS) - 1)
+
+#define MAJOR(dev) ((unsigned int) ((dev) >> MINORBITS))
+#define MINOR(dev) ((unsigned int) ((dev) &MINORMASK))
+
+#define RWBS_LEN 8
+
+/* Note: the parameter of a tracepoint can be  by the
+ * first arguments of DEFINE_EVENT(block_rq, block_io_start, ...) */
+SEC("tracepoint/block/block_io_start")
+int block_rq_insert(struct trace_event_raw_block_rq *args)
 {
-    bpf_printk("block_rq_insert");
+    u64 ts = bpf_ktime_get_ns();
+    dev_t dev = args->dev;
+    char *rwbs = args->rwbs;
+    sector_t sector = args->sector;
+
+    u32 dev_major = MAJOR(dev);
+    u32 dev_minor = MINOR(dev);
+
+    bpf_printk("[%-10lld] block_io_start: (%-3d,%-3d) %-8s %-10d", ts,
+               dev_major, dev_minor, rwbs, sector);
+
     return 0;
 }
 
