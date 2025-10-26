43a36d7b1de1034b0754aa2ff6f3b556a1eb24d7
Yiwei Lin <s921975628@gmail.com>
2025-10-27 00:28:46 +0800

    Store bio information in hashmap when I/O start
    Then we can match it when I/O done to evaluate the
    time consumption for each I/O operation.

--- bpf/biomon.bpf.c
+++ bpf/biomon.bpf.c
@@ -18,22 +18,67 @@
 
 #define RWBS_LEN 8
 
+typedef struct {
+    dev_t dev;
+    u32 rwflag;
+    sector_t sector;
+} hash_key_t;
+
+typedef struct {
+    u64 ts;
+    u64 data_len;
+} req_t;
+
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, 10240);
+    __type(key, hash_key_t);
+    __type(value, req_t);
+} req_info SEC(".maps");
+
 /* Note: the parameter of a tracepoint can be  by the
  * first arguments of DEFINE_EVENT(block_rq, block_io_start, ...) */
 SEC("tracepoint/block/block_io_start")
 int block_io_start(struct trace_event_raw_block_rq *args)
 {
     u64 ts = bpf_ktime_get_ns();
+    u32 rwflag = 0;
+    u32 dev_major, dev_minor;
+    hash_key_t key;
+    req_t req;
     dev_t dev = args->dev;
-    char *rwbs = args->rwbs;
+    char rwbs[RWBS_LEN];
     sector_t sector = args->sector;
+    int i = 0;
 
-    u32 dev_major = MAJOR(dev);
-    u32 dev_minor = MINOR(dev);
+    bpf_probe_read(&rwbs, RWBS_LEN, args->rwbs);
+
+    while (i < RWBS_LEN && rwbs[i] != '\0') {
+        if (rwbs[i] == 'W') {
+            rwflag = 1;
+            break;
+        }
+        i++;
+    }
+
+    dev_major = MAJOR(dev);
+    dev_minor = MINOR(dev);
 
     bpf_printk("[%-10lld] block_io_start: (%-3d,%-3d) %-8s %-10d", ts,
                dev_major, dev_minor, rwbs, sector);
 
+    key = (hash_key_t){
+        .dev = dev,
+        .rwflag = rwflag,
+        .sector = sector,
+    };
+
+    req = (req_t){
+        .ts = ts,
+        .data_len = 0,
+    };
+
+    bpf_map_update_elem(&req_info, &key, &req, BPF_ANY);
     return 0;
 }
 
