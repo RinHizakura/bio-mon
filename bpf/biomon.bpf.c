b1fd2bde8d3de83a0e746fc99dad1194c97fc202
Yiwei Lin <s921975628@gmail.com>
2025-11-11 01:12:59 +0800

    Correct the pid and task comm reading

--- bpf/biomon.bpf.c
+++ bpf/biomon.bpf.c
@@ -25,14 +25,15 @@ typedef struct {
 
 typedef struct {
     u64 ts;
-    u64 data_len;
-} req_t;
+    u32 pid;
+    char comm[TASK_COMM_LEN];
+} val_t;
 
 struct {
     __uint(type, BPF_MAP_TYPE_HASH);
     __uint(max_entries, 10240);
     __type(key, hash_key_t);
-    __type(value, req_t);
+    __type(value, val_t);
 } req_info SEC(".maps");
 
 struct {
@@ -62,10 +63,14 @@ int block_io_start(struct trace_event_raw_block_rq *args)
     u64 ts = bpf_ktime_get_ns();
     u32 rwflag;
     hash_key_t key;
-    req_t req;
+    val_t val;
     dev_t dev = args->dev;
     char rwbs[RWBS_LEN];
     sector_t sector = args->sector;
+
+    /* Ignore this I/O if it fails to get its pid */
+    if (bpf_get_current_comm(&val.comm, sizeof(val.comm)))
+        return 0;
 
     bpf_probe_read(&rwbs, RWBS_LEN, args->rwbs);
     rwflag = get_rwflag_tp(rwbs);
@@ -76,26 +81,23 @@ int block_io_start(struct trace_event_raw_block_rq *args)
         .sector = sector,
     };
 
-    req = (req_t){
-        .ts = ts,
-        .data_len = 0,
-    };
+    val.ts = ts;
+    val.pid = bpf_get_current_pid_tgid() >> 32;
 
-    bpf_map_update_elem(&req_info, &key, &req, BPF_ANY);
+    bpf_map_update_elem(&req_info, &key, &val, BPF_ANY);
     return 0;
 }
 
 SEC("tracepoint/block/block_io_done")
 int block_io_done(struct trace_event_raw_block_rq *args)
 {
-    pid_t pid = (bpf_get_current_pid_tgid() >> 32);
     u64 ts = bpf_ktime_get_ns();
     u32 rwflag;
     dev_t dev = args->dev;
     char rwbs[RWBS_LEN];
     sector_t sector = args->sector;
     hash_key_t key;
-    req_t *req;
+    val_t *val;
     msg_ent_t *ent;
 
     bpf_probe_read(&rwbs, RWBS_LEN, args->rwbs);
@@ -107,26 +109,21 @@ int block_io_done(struct trace_event_raw_block_rq *args)
         .sector = sector,
     };
 
-    req = bpf_map_lookup_elem(&req_info, &key);
-    if (req) {
-        ts -= req->ts;
+    val = bpf_map_lookup_elem(&req_info, &key);
+    if (val) {
+        ent = bpf_ringbuf_reserve(&msg_ringbuf, sizeof(msg_ent_t), 0);
+        if (ent) {
+            ent->id = MSG_ID++;
+            ent->ts = ts - val->ts;
+            ent->pid = val->pid;
+            ent->sector = sector;
+            ent->dev = dev;
+            ent->rwflag = rwflag;
+            memcpy(ent->comm, val->comm, sizeof(val->comm));
+            bpf_ringbuf_submit(ent, 0);
+        }
+
         bpf_map_delete_elem(&req_info, &key);
-    } else
-        ts = 0;
-
-    bpf_printk("(%-3d,%-3d) %-8s %-10d time=%-10d(ms)", MAJOR(dev), MINOR(dev),
-               rwbs, sector, ts / 1000);
-
-    ent = bpf_ringbuf_reserve(&msg_ringbuf, sizeof(msg_ent_t), 0);
-    if (ent) {
-        ent->id = MSG_ID++;
-        ent->ts = ts;
-        ent->pid = pid;
-        ent->sector = sector;
-        ent->dev = dev;
-        ent->rwflag = rwflag;
-        bpf_get_current_comm(&ent->comm, sizeof(ent->comm));
-        bpf_ringbuf_submit(ent, 0);
     }
 
     return 0;
